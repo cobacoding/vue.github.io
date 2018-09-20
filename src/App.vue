afdad48e5a8d0d7f277a7e8672308bdc2b40980b
cobacoding <belajarcoding@yahoo.com>
2018-09-20 07:00:13 +0700

    tes

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <!-- <img src="./assets/logo.png"> -->
+    <Navbar/>
+    <div class="container">
+    	<router-view/>
+    </div>
+  </div>
+</template>
+
+<script>
+import Navbar from './components/NavBar'
+export default {
+  name: 'app',
+  components: {
+  	Navbar
+  }
+}
+</script>
+
---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+	 apiKey: "AIzaSyCX7ZxSTPK2b4X7h2Ow0gNufP3hmu-eJgo",
+    authDomain: "empmanager-a2e36.firebaseapp.com",
+    databaseURL: "https://empmanager-a2e36.firebaseio.com",
+    projectId: "empmanager-a2e36",
+    storageBucket: "empmanager-a2e36.appspot.com",
+    messagingSenderId: "451632063295"
+}
